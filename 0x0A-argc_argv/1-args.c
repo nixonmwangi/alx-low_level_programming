08c02acd3433d8c7b1ca87fd44eff42a1972dd6c
nixonmwangi <nixonmwangi4870@gmail.com>
2023-09-29 12:43:39 -0700

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * @argc: count
+ * @argv: arguments string.
+ * Return: always 0 on success.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
