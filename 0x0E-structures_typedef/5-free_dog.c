cbc957f7fb416be65b59f7e5536e6735afbcc55b
nixonmwangi <nixonmwangi4870@gmail.com>
2023-10-18 05:16:52 -0700

    c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,59 +2,17 @@
 #include <stdlib.h>
 
 /**
- * _strcopy - copy read only data to mutatable.
- * @dst: pointer to copy char to.
- * @src: read only data.
- */
-void _strcopy(char *dst, char *src)
-{
-	int i;
-
-	for (i = 0; src[i]; i++)
-		dst[i] = src[i];
-	dst[i] = '\0';
-}
-
-/**
- * new_dog - create new dog from the dna of the first dog.
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * free_dog - free(memory) dah dog and get fined by animal control
+ * @d: pointer to dog stuct in memory.
  *
- * Return: pointer to dog
+ * Return: nothing
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-	dog_t *d;
-	int a, b;
-
-	for (a = 0; name[a]; a++)
-		;
-	for (b = 0; owner[b]; b++)
-		;
-
-	d = malloc(sizeof(dog_t));
 	if (!d)
-		return (NULL);
-
-	d->name = malloc(a + 1);
-	if (!d->name)
-	{
-		free(d);
-		return (NULL);
-	}
-
-	d->owner = malloc(b + 1);
-	if (!d->owner)
-	{
-		free(d->name);
-		free(d);
-		return (NULL);
-	}
-
-	_strcopy(d->name, name);
-	_strcopy(d->owner, owner);
-	d->age = age;
-	return (d);
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
 
